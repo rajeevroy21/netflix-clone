3f5842848d73e68c456b04ab3b322a3ed5402627
Rajeev kumar <rajeevroy70701@gmail.com>
2025-04-09 19:26:14 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -19,8 +19,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://netflix-clone-git-main-rajeevroy21s-projects.vercel.app',
-    credentials:true
+    origin:['https://netflix-clone-rajeev.onrender.com','http://localhost:5173']
 }
 app.use(cors(corsOptions));
  
